feee8582ef58cbd947ef796ed07990e6ca225a6a
Stephen Omoregie <cr8stevestudios@gmail.com>
2024-06-06 21:13:51 +0100

    Function and variable annotations

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Basic Annotations - To String
+"""
+
+
+def to_str(n: float) -> str:
+    """Type annotated function to_str that takes
+    a float n as an argument and returns the string
+    representation.
+    """
+    return str(n)
---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Defining type-annotated variables
+This is however redundant for directly assigned
+variables as the type of the variable is automatically
+inferred
+"""
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
