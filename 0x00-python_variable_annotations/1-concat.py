11a871d5813d24d4c3516f17dc10bb35baff04b8
Stephen Omoregie <cr8stevestudios@gmail.com>
2024-06-06 21:03:00 +0100

    Type annotating a concatenation function

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Basic Annotations for a function named concat.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Type annotated function concat that takes a string
+    str1 and str2 as arguments and returns a
+    concatenated string
+    """
+    return str1 + str2
