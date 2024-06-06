86a0d0b262a4a9657c821c06afa15cd6e03b1e52
Stephen Omoregie <cr8stevestudios@gmail.com>
2024-06-06 20:53:42 +0100

    Python Typing and Annotation

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Basic Annotations for a function named add.
+"""
+
+def add(a:float, b:float)->float:
+    """A Type annotated function that takes a float 'a'
+    and float 'b' as arguments and returns their sum 
+    as a float
+    """
+    return a + b
---
+++ README.md
@@ -0,0 +1,4 @@
+# ALX Backend Python
+
+This is the repository housing sub directories of concepts and mini tasks undertaken in learning Python for Backend Specialization.
+
