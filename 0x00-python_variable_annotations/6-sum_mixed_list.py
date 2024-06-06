bbdad20a0651c05ea0b7b128405f6b8fbcd40d30
Stephen Omoregie <cr8stevestudios@gmail.com>
2024-06-06 21:30:36 +0100

    Remove breaking pycodestyle - trailing whitespace

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -8,7 +8,7 @@ from typing import List, Union
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """
     Type-annotated function sum_mixed_list which takes
-    a list mxd_lst of integers and floats and 
+    a list mxd_lst of integers and floats and
     returns their sum as a float
     """
     return float(sum(mxd_lst))
