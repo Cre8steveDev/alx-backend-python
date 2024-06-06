2991564c35a97f2c976f5964494b1fae91cfb7dd
Stephen Omoregie <cr8stevestudios@gmail.com>
2024-06-06 21:29:19 +0100

    Using union from typing module

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -2,10 +2,10 @@
 """
 Complex types - Mixed Lists, Union
 """
-from typing import List
+from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[int | float]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """
     Type-annotated function sum_mixed_list which takes
     a list mxd_lst of integers and floats and 
