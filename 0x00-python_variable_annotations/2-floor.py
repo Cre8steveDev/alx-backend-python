2074e18217db20639de4af2757c19f08da64e3e3
Stephen Omoregie <cr8stevestudios@gmail.com>
2024-06-06 21:07:44 +0100

    Correction - using type conversion to coerce into an int

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -8,4 +8,4 @@ def floor(n: float) -> int:
     """Type annotated function floor which takes a float n
     as argument and returns the floor of the float
     """
-    return n // 1
+    return int(n)
