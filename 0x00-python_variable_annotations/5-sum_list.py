e969eb454492ca5de455456e0f29c09fe74444b1
Stephen Omoregie <cr8stevestudios@gmail.com>
2024-06-06 21:23:35 +0100

    Added List from typing module|

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -2,9 +2,10 @@
 """
 Complex types - list of floats
 """
+from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """
     Type-annotated function sum_list which
     takes a list input_list of floats as
